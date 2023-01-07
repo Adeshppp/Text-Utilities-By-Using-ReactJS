3c220207eeadedb387f2e20e8aa356bb8fb432c8
Adesh <adeshp948@gmail.com>
2023-01-07 12:12:41 -0500

    adding files

---
+++ src/My Components/Navbar.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
+export default function Navbar(props) {
+  return (
+
+    <>
+      <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
+        <Link className="navbar-brand  mx-3" to="/home">{props.title}</Link>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item active">
+              <Link className="nav-link" to="/home">Home</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/about">About</Link>
+            </li>
+
+
+          </ul>
+
+        </div>
+        <div className="form-check form-switch mx-3">
+          <input className="form-check-input" onClick={props.toggleMode} type="checkbox" role="switch" id="flexSwitchCheckDefault" />
+
+          <label className={`form-check-label text-${props.mode==="dark"?"light":"dark"}`} for="flexSwitchCheckDefault">Enable {props.mode==="dark"?"light":"dark"} Mode</label>
+        </div>
+      </nav>
+    </>
+  )
+}
+
+
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired,
+  about: PropTypes.string.isRequired
+}
+
+Navbar.defaultProps = {
+  title: "Title",
+  about: "About"
+}
